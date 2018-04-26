4121254b55b8dbcc719f8f05fa27488f27cf0010
Gaëlle <gibier.gaelle@hotmail.fr>
2018-04-26 09:31:43 +0200

    fin du projet

---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Form = props => (
+
+  <form onSubmit={ props.getWeather }>
+    <input type="text" name="city" placeholder="Ville..." />
+    <input type="text" name="country" placeholder="Pays..." />
+    <button>Météo</button>
+  </form>
+  
+)
+
+
+export default Form
---
+++ src/components/Titles.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Titles = props => (
+  <div>
+    <h1 className='title-container__title'>Météo en direct</h1>
+    <p className='title-container__subtitle'>Toutes les informations nécessaires sur le lieu souhaité</p>
+  </div>
+)
+export default Titles
